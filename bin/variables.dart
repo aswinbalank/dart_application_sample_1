47809327106b309f0dcb42e92f2fe6fad64b6831
aswinbalank <aswinbalank32@gmail.com>
2023-11-06 14:21:50 +0530

    commit

---
+++ bin/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "aswin";
+  String email = "aswinbalank32@gmail.com";
+  int age = 20;
+  print("my name is $name");
+  print("my email is $email");
+  print("my age is $age");
+
+  int a = 20, b = 10;
+  print("sum is ${a + b}");
+}
